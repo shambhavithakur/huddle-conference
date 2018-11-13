dc348b8ec9cbd365482aaea72d0372a481c6a2aa
Shambhavi <reply2112@gmail.com>
2018-11-13 10:06:43 +0530

    Finalize header section

--- js/script.js
+++ js/script.js
@@ -4,28 +4,40 @@ var navBarNotFixed = document.querySelector('.navbar');
 var navLogoContainer = navBarNotFixed.querySelector('.nav-container > .logo-container');
 var navListOpen = document.querySelector('.header-navlist-container');
 var primaryInfoP = document.querySelector('.primary-info > p');
-var primaryInfoSpans = document.querySelectorAll('.primary-info > h1 > span');
+var primaryInfoH1Span = document.querySelector('.primary-info > h1 > span');
+var scheduleLocation = document.querySelector('.primary-info > .schedule-loc');
+var organizedBy = document.querySelector('.primary-info > .organized-by');
+var getTickets = document.querySelector('.primary-info > .tickets-container');
+var ticketAvailability = document.querySelector('.primary-info > .ticket-availability');
 
-
-// Local Storage get
+// Animation effects should not be repeated after they have played once and the user has scrolled past the header.
+function removeEffects(){
+    navLogoContainer.classList.remove('opacity');
+    navLogoContainer.classList.remove('slide-in-from-top');
+    primaryInfoP.classList.remove('opacity');
+    primaryInfoP.classList.remove('slide-in-from-bottom');
+    primaryInfoH1Span.classList.remove('opacity');
+    primaryInfoH1Span.classList.remove('slide-in-from-bottom');
+    scheduleLocation.classList.remove('opacity');
+    scheduleLocation.classList.remove('slide-in-from-bottom');
+    organizedBy.classList.remove('opacity');
+    organizedBy.classList.remove('slide-in-from-bottom');
+    getTickets.classList.remove('opacity');
+    getTickets.classList.remove('slide-in-from-bottom');
+    ticketAvailability.classList.remove('opacity');
+    ticketAvailability.classList.remove('slide-in-from-bottom');
+    document.querySelector('.geometric1').classList.remove('opacity');
+    document.querySelector('.geometric1').style.animationName = 'none';
+    document.querySelector('.geometric2').classList.remove('opacity');
+    document.querySelector('.geometric2').style.animationName = 'none';
+    document.querySelector('.geometric3').classList.remove('opacity');
+    document.querySelector('.geometric3').style.animationName = 'none';
+    document.querySelector('.geometric4').classList.remove('opacity');
+    document.querySelector('.geometric4').style.animationName = 'none';
+}
 var accessedOrNot = localStorage.getItem('pageAccessed');
 if (accessedOrNot) {
-  navLogoContainer.classList.remove('opacity');
-  navLogoContainer.classList.remove('slide-in-from-top');
-  primaryInfoP.classList.remove('opacity');
-  primaryInfoP.classList.remove('slide-in-from-bottom');
-  for (var i = 0; i < primaryInfoSpans.length; i++) {
-    primaryInfoSpans[i].classList.remove('opacity');
-    primaryInfoSpans[i].classList.remove('slide-in-from-bottom');
-  }
-  document.querySelector('.geometric1').classList.remove('opacity');
-  document.querySelector('.geometric1').style.animationName = 'none';
-  document.querySelector('.geometric2').classList.remove('opacity');
-  document.querySelector('.geometric2').style.animationName = 'none';
-  document.querySelector('.geometric3').classList.remove('opacity');
-  document.querySelector('.geometric3').style.animationName = 'none';
-  document.querySelector('.geometric4').classList.remove('opacity');
-  document.querySelector('.geometric4').style.animationName = 'none';
+  removeEffects();
 }
 
 // Toggler
@@ -74,3 +86,12 @@ function fixedNav() {
 }
 
 window.addEventListener('scroll', fixedNav);
+
+// Replace animation with static elements when user begins to scroll.
+function noAnimationAfterScroll(){
+    var presentPosition = window.pageYOffset;
+    if (presentPosition > 500){
+        removeEffects();
+    }
+}
+window.addEventListener('scroll', noAnimationAfterScroll);
